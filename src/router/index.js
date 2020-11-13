7d0e9d8cc3daa438bd1e3ffdeb7cb27f36ec58f5
Anan Hartanto <hartantoaanan@gmail.com>
2020-11-13 07:38:57 +0700

    simple leaderboard from table

--- src/router/index.js
+++ src/router/index.js
@@ -3,32 +3,32 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    children: [
-      {
-        path: '',
-        name: 'room',
-        component: () => import('../components/dashboard/Dashboard.vue')
-      },
-      {
-        path: 'rooms/:id',
-        name: 'room',
-        component: () => import('../components/room/Room.vue')
-      }
-    ]
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: () => import('../views/Home.vue'),
+  children: [{
+    path: '',
+    name: 'room',
+    component: () => import('../components/dashboard/Dashboard.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: 'rooms/:id',
+    name: 'room',
+    component: () => import('../components/room/Room.vue')
   }
+  ]
+},
+{
+  path: '/about',
+  name: 'About',
+  component: () => import('../views/About.vue')
+},
+{
+  path: '/game-over',
+  name: 'Game Over',
+  component: () => import('../components/GameOver.vue')
+}
 ]
 
 const router = new VueRouter({
